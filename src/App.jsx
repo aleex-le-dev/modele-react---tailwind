f6ff39f775266050aa52e233e902d45c0c323904
aleex-le-dev <jalias59@outlook.fr>
2025-08-24 18:30:20 +0200

    Modification du style du titre dans App.jsx pour inclure une couleur rouge et centrer le texte.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-        <h1 className="text-3xl font-bold underline">Modele react + tailwind</h1>
+        <h1 className="text-3xl font-bold underline text-red-500 text-center">Modele react + tailwind</h1>
     </>
   );
 }
